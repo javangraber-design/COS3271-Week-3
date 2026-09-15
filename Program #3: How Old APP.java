6d05912fde5d92723d2d6e4cb0db3907b211a37c
Javan Graber <jmgraber@students.unwsp.edu>
2026-09-14 21:24:35 -0500

    Create Program #3: How Old APP.java

---
+++ Program #3: How Old APP.java
@@ -0,0 +1,27 @@
+//Programmer: Javan Graber
+//Date: 9/14/26
+
+package javanproject;
+
+import java.util.Scanner;
+public class MyProject {
+    static Scanner userinput = new Scanner(System.in);
+    public static void main(String[] args) {
+    	
+    	//Create the variable
+        double age, futureAge;
+        
+        //Get the user input
+        System.out.print("How old are you? (Enter as a number) --> ");
+        age = userinput.nextDouble();
+        futureAge = age + 5;
+        
+        //Perform the calculations and print
+        System.out.print("\n");
+        System.out.println("\tIn 5 years you will be " + futureAge + " years old.");
+        System.out.println("\tIt is " + (age < 20 && age >= 13) +" that you are a teenager");
+        System.out.println("\tI am such a smart computer.");
+
+    }
+
+}
